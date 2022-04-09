12e26c2f4b2be39c502ea8107367e2eebda0fddb
gaurav <gauravpratapsingh1071997@gmail.com>
2022-04-10 00:49:42 +0530

    chnages

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import {
   Route,
 } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
+// import Login from "./pages/Login/Login";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
+          {/* <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
@@ -21,8 +21,8 @@ function App() {
 
 export default App;
 
-const About = () => {
-  return <div>About</div>
-}
+// const About = () => {
+//   return <div>About</div>
+// }
 
 
