48847bee09f1b3a167bdec0d6fbdf10870b69711
gaurav <gauravpratapsingh1071997@gmail.com>
2022-05-08 19:50:27 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Home from "./pages/Home/Home";
-// import Login from "./pages/Login/Login";
+import { Provider } from "react-redux";
+
+import store from "./store/store";
+
+
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './reducer/counterReducer'
+
+
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          {/* <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} /> */}
-        </Routes>
-      </BrowserRouter>
+     <Provider store={store}>
+        <Counter />
+     </Provider>
     </div>
   );
 }
 
 export default App;
 
-// const About = () => {
-//   return <div>About</div>
-// }
 
 
+
+ function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+
+
+
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(incrementByAmount(33))}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
